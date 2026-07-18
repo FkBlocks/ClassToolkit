fa92ce55058709c1f55d8e0a463479fd3ca7706d
obsidian <fangk1589969@outlook.com>
2026-07-18 20:47:44 +0800

    完善随机点名名单读取，更人性化设计

--- ClassToolkit.RandomName/MainWindow.xaml.cs
+++ ClassToolkit.RandomName/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using ClassToolkit.Core.Controls;
 using ClassToolkit.Core.Services;
+using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace ClassToolkit.RandomName;
 
@@ -10,6 +13,8 @@ public partial class MainWindow : CustomWindow
         LogService.Init("RandomName");
         InitializeComponent();
         UpdateDisplay();
+        LoadName();
+        LogService.Info($"成功启动随机点名，名字数量：{namesCount}");
     }
 
     /// <summary>
@@ -18,8 +23,17 @@ public partial class MainWindow : CustomWindow
     private const int DEFAULT_COUNT = 1;
     private const int MIN_COUNT = 1;
     private string namesPath = ClassToolkit.Core.Utilities.DataPathHelper.GetDataPath("names.txt");
+    private readonly List<string> DefaultName = new List<string>
+    {
+        "张三", "李四", "王五", "赵六",
+        "小明", "小红", "小芳", "小虎"
+    };
+
+    public List<string>? names;
+    public int namesCount = 0;
 
     private int _count = DEFAULT_COUNT;
+
 
     /// <summary>
     /// 减法按钮按下，计数器减一
@@ -51,8 +65,42 @@ public partial class MainWindow : CustomWindow
         ChoiceTimes.Text = _count.ToString();
     }
 
+    // TODO 点名逻辑
+    /// <summary>
+    /// 点名逻辑
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void Call(object sender, System.Windows.RoutedEventArgs e)
     {
 
+    }
+
+    private void LoadName()
+    {
+
+        try
+        {
+            names = File.ReadAllLines(namesPath, Encoding.UTF8).ToList();
+            if (names.SequenceEqual(DefaultName))
+            {
+                LogService.Info("使用默认名单");
+                MessageBox.Show("你当前使用的是默认名单！\n" +
+                    "这不会影响程序的使用，但请前往设置填写或导入本班名单\n" +
+                    "你也可以在 程序目录/data/names.txt 文件中手动填写，确保每一行一个名字",
+                    "默认名单", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            namesCount = names.Count;
+
+        }
+        catch (Exception ex)
+        {
+            LogService.Error($"读取名单错误：{ex}");
+            MessageBox.Show($"读取名单发生致命错误：{ex}。\n 详情见 程序目录/data/log/running.log 日志文件", "错误", 
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            Application.Current.Shutdown(1);
+            return;
+        }
     }
 }
